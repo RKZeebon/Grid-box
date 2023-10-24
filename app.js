096e144cd1d19a35b156389f5247a3652b17d259
Md. Rezaul Karim <96794203+RKZeebon@users.noreply.github.com>
2023-10-24 14:46:38 +0600

    Added animation with color changes

--- app.js
+++ app.js
@@ -5,26 +5,19 @@ const h = () => Math.round(Math.random() * 1000)
 const s = () => Math.round(Math.random() * 100)
 const l = () => {
     const gotedValue = Math.round(Math.random() * 100)
-    if (gotedValue > 80) {
-        return 80
-    }
-    else if (gotedValue < 20) {
-        return 20
-    }
-    else return gotedValue
-
+    return gotedValue > 80 ? 80 : gotedValue < 20 ? 20 : gotedValue
 }
 
 const changeColor = () => {
-    roots.style.setProperty('--text', `hsl(${h()}, ${s()}%, ${l() > 30 && 30}%)`)
+    roots.style.setProperty('--text', `hsl(${h()}, ${s()}%, ${l()}%)`)
     roots.style.setProperty('--textTwo', `hsl(${h()}, ${s()}%, ${l()}%)`)
     roots.style.setProperty('--background', `hsl(${h()}, ${s()}%, ${l()}%)`)
     roots.style.setProperty('--primary', `hsl(${h()}, ${s()}%, ${l()}%)`)
     roots.style.setProperty('--secondary', `hsl(${h()}, ${s()}%, ${l()}%)`)
     roots.style.setProperty('--tertiary', `hsl(${h()}, ${s()}%, ${l()}%)`)
     roots.style.setProperty('--accent', `hsl(${h()}, ${s()}%, ${l()}%)`)
-    roots.style.setProperty('--btnBgc', `hsl(${h()}, ${s()}%, ${l() < 50 && 50}%)`)
+    roots.style.setProperty('--btnBgc', `hsl(${h()}, ${s()}%, ${l()}%)`)
 }
 
 btn.addEventListener('click', changeColor)
-setInterval(changeColor, 4000)
+setInterval(changeColor, 3000)
